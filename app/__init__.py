d78e5d33edf6b962fb66f624ac5fb5a2bf368e70
NessaAz <vanessa.azenwa@gmail.com>
2022-05-09 20:29:08 +0300

    add create app definition

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
-#from flask_mail import Mail
-#from flask_login import LoginManager
+from flask_mail import Mail
+from flask_login import LoginManager
 
 #Definitions
 bootstrap = Bootstrap()
