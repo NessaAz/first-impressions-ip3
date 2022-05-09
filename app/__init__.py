d026b4398e10f06a402778e5078023017adb1a2e
NessaAz <vanessa.azenwa@gmail.com>
2022-05-09 20:20:29 +0300

    add create_app definition to initialize app

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from ensurepip import bootstrap
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
+#from flask_mail import Mail
+#from flask_login import LoginManager
+
+#Definitions
+bootstrap = Bootstrap()
+
+#initializing application
+def create_app(config_name):
+    app = Flask(__name__ )
+    
+    #Creating the app configurations
+    app.config.from_object (config_options[config_name])
+    
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+    
+    
+    return app
