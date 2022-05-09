4c7b5d0808d59d440fafc516fabe9e3b7c0f818c
NessaAz <vanessa.azenwa@gmail.com>
2022-05-09 20:18:44 +0300

    add blueprint import

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,21 +1,2 @@
-from ensurepip import bootstrap
-from flask import Flask
-from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from config import config_options
-
-#Definitions
-bootstrap = Bootstrap()
-
-#initializing application
-def create_app(config_name):
-    app = Flask(__name__ )
-    
-    #Creating the app configurations
-    app.config.from_object (config_options[config_name])
-    
-    # Initializing Flask Extensions
-    bootstrap.init_app(app)
-    
-    
-    return app
+from flask import Blueprint
+main = Blueprint("main" , __name__)
