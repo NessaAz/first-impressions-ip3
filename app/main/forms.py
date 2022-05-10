1f260fbd69dff9c4d66f75cd8cb995570d5eca48
NessaAz <vanessa.azenwa@gmail.com>
2022-05-10 23:00:07 +0300

    modify form validations

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,15 +3,15 @@ from wtforms import StringField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Write a brief bio about you.',validators = [DataRequired()])
-    submit = SubmitField('Save')
+    bio = TextAreaField('describe yourself',validators = [DataRequired()])
+    submit = SubmitField('save')
 
 class PitchForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    category = SelectField('Category', choices=[('Events','Events'),('Job','Job'),('Advertisement','Advertisement')],validators=[DataRequired()])
-    post = TextAreaField('Your Pitch', validators=[DataRequired()])
+    title = StringField('title', validators=[DataRequired()])
+    category = SelectField('category', choices=[('Jobs','Jobs'),('Event','Event'),('Dates','Dates')],validators=[DataRequired()])
+    post = TextAreaField('your pitch', validators=[DataRequired()])
     submit = SubmitField('Pitch')
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField('Leave a comment',validators=[DataRequired()])
-    submit = SubmitField('Comment')
+    comment = TextAreaField('comment',validators=[DataRequired()])
+    submit = SubmitField('comment')
