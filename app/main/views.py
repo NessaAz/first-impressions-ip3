7b5cc68f0144952bbd32e5ff2e3b7cba2e04df24
NessaAz <vanessa.azenwa@gmail.com>
2022-05-10 22:13:59 +0300

    add view function to pitch html

--- app/main/views.py
+++ app/main/views.py
@@ -15,3 +15,19 @@ def index():
     return render_template('index.html', allp = allp,jobs = jobs, event = event,dates= dates)
 
 
+@main.route('/create_new', methods = ['POST','GET'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user
+        new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_pitch_object.save_p()
+        return redirect(url_for('main.index'))
+        
+    return render_template('create_pitch.html', form = form)
+
+
