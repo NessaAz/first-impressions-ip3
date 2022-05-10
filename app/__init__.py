b051757c96e6b6db49ac4285cd648af2730b7654
NessaAz <vanessa.azenwa@gmail.com>
2022-05-10 20:37:26 +0300

    add view file for log in and sin up authorization

--- app/__init__.py
+++ app/__init__.py
@@ -11,11 +11,18 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 mail = Mail()
 login_manager = LoginManager()
+# sqlalchemy = SQLAlchemy()
+#sqlalchemy.create_app(create_app)
 
 
 #initializing application
 def create_app(config_name):
     app = Flask(__name__ )
+    db.init_app(app)
+    
+    # from models import User
+    # with app.app_context():
+    #     db.create_all()
     
     #Creating the app configurations
     app.config.from_object (config_options[config_name])
