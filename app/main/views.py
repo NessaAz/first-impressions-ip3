292e4c36116f3e199ab294a52c7a34056c0044d0
NessaAz <vanessa.azenwa@gmail.com>
2022-05-10 22:01:55 +0300

    add render template to pitch categories

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,17 @@
-from flask  import render_template
+from flask import render_template, redirect, url_for,abort,request
 from . import main
+from flask_login import login_required,current_user
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .forms import UpdateProfile,PitchForm,CommentForm
+# from .. import db
 
 
 @main.route('/')
 def index():
-    
-    return render_template('index.html')
+    allp = Pitch.query.all()
+    jobs = Pitch.query.filter_by(category = 'Interviews').all() 
+    event = Pitch.query.filter_by(category = 'Networking').all()
+    dates = Pitch.query.filter_by(category = 'Dates').all()
+    return render_template('index.html', allp = allp,jobs = jobs, event = event,dates= dates)
+
+
