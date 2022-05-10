41529793e9b40d8dbe9b120ac800abe383dd60de
NessaAz <vanessa.azenwa@gmail.com>
2022-05-10 18:34:36 +0300

    add confuguration mail auths, secret key

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+load_dotenv() 
 
+#os.urandom(20)
 
 
 class Config:
@@ -7,12 +10,21 @@ class Config:
     Parent class
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/images'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig:
     '''
     Child to Config class
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig:
     '''
